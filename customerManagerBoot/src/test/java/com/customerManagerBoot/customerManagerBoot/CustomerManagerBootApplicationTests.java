b5794d323342870ce2022b98a8daa145c142379d
rafamDev <76625223+rafamDev@users.noreply.github.com>
2021-05-07 21:53:44 +0200

    jjjweb

---
+++ customerManagerBoot/src/test/java/com/customerManagerBoot/customerManagerBoot/CustomerManagerBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customerManagerBoot.customerManagerBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerManagerBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
